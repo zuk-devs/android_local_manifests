aaecd9fb734ece1d9b9d3c388e54b654585651f6
Davide Garberi <dade.garberi@gmail.com>
2017-12-12 12:56:54 +0100

    repopicker: Update to repopick only
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- repopicker.sh
+++ repopicker.sh
@@ -6,7 +6,5 @@ repopick 198029
 # ntfs-3g
 repopick 198028
 
-# lineagehw dummy
-repopick 198037
-
-cd vendor/lineage && git fetch https://review.lineageos.org/LineageOS/android_vendor_lineage refs/changes/52/192352/5 && git cherry-pick FETCH_HEAD && cd ../..
+# vendor LA.UM
+repopick 198617 198614
