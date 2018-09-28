9d80a9e12a68f0d3b70425c89cea61df9e85d2b8
Davide Garberi <dade.garberi@gmail.com>
2018-09-28 16:43:05 +0200

    local_manifest: Update
    * Add navbar and buttons commits repopicks
    * Add a repopick to get power 1.1 to work
    * Sync the setupwizard to not make the repopick fail
    
    Signed-off-by: Davide Garberi <dade.garberi@gmail.com>

--- repopick.sh
+++ repopick.sh
@@ -5,3 +5,12 @@ repopick -t pie-bt-caf
 
 # Sepolicy
 repopick -t pie-qcom-sepolicy
+
+# Navbar
+repopick -t pie-navbar-runtime-toggle
+
+# Buttons
+repopick -t pie-buttons
+
+# PowerHAL
+repopick 229339
